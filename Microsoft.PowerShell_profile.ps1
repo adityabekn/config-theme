36bb19830ddc8136dcd4772066b99c3017a1ad6c
Aditya Bekti N <34529142+adityabekn@users.noreply.github.com>
2024-11-27 01:05:36 +0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,19 @@
 Invoke-Expression "$(vfox activate pwsh)"
-
-oh-my-posh init pwsh --config ~/ys.omp.json | Invoke-Expression
-
+Import-Module -Name Terminal-Icons
+Import-Module PSReadLine
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Key Tab -Function Complete
+Set-PSReadLineOption -PredictionSource History
 Set-Alias lzg lazygit
+function prompt {
+  $loc = $executionContext.SessionState.Path.CurrentLocation;
+
+  $out = ""
+  if ($loc.Provider.Name -eq "FileSystem") {
+    $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+  }
+  $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
+  return $out
+}
+oh-my-posh init pwsh --config ~/ys.omp.json | Invoke-Expression
